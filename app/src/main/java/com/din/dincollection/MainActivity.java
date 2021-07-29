06f70096fb56f825333c3af1e62dd467bc39aa94
ddin <fengjie@hytto.com>
2021-07-29 12:35:38 +0800

    3

--- app/src/main/java/com/din/dincollection/MainActivity.java
+++ app/src/main/java/com/din/dincollection/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String a ="master";
+        String a ="master2";
        String b = "dev";
     }
 }
